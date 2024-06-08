11c9e6b26b03f1cbf66d3755c5676efa938a7a08
Lakshya <Lakshya>
2024-06-08 12:46:16 +0530

    removing firebase

--- Front4nd/Strift_frontend_login/src/Components/Login flow/login1.jsx
+++ Front4nd/Strift_frontend_login/src/Components/Login flow/login1.jsx
@@ -6,8 +6,6 @@ import "react-phone-input-2/lib/style.css";
 import { Link } from "react-router-dom";
 
 import { useEffect } from "react";
-import { auth, provider } from "./config";
-import { signInWithPopup } from "firebase/auth";
 import ProfileName from "./profileName";
 
 function Login1() {
